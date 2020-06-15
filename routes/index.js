a9c75f19fbe7729b5f934a7b07e59409a13d47c3
hllywdunddnnpnt <hllywdunddnnpnt@hotmail.com>
2020-06-15 17:08:20 +0800

    work end

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/index", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/hello", (req, res) => {
+  res.send("hello!");
 });
 
 module.exports = router;
