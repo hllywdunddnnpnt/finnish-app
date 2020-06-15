9dd1bf562ba3c3b8225afcecfa0e34ae869634b8
hllywdunddnnpnt <hllywdunddnnpnt@hotmail.com>
2020-06-15 13:08:05 +0800

    just a psh

--- app.js
+++ app.js
@@ -4,10 +4,24 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var livereload = require('livereload');
+var connectLivereload = require('connect-livereload');
+const public = path.join(__dirname, 'public');
+
+liveReloadServer = livereload.createServer();
+liveReloadServer.watch(public);
+liveReloadServer.server.once('connection', () => {
+  setTimeout(() => {
+    liveReloadServer.refresh('/');
+  }, 100);
+});
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(connectLivereload());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,7 +31,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(public));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
